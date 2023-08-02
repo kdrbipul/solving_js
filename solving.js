a1a43f98fa0a76a6ff884ce828162ac9fa755156
kdrbipul <kdrbipul@gmail.com>
2023-08-02 12:19:33 +0600

    first commit

---
+++ solving.js
@@ -0,0 +1,79 @@
+// function smallestNumber(numbersOfArray){
+//     //  calculate korbo smallest number
+//     let smallest=numbersOfArray[0];
+//     for(i=0;i<numbersOfArray.length;i++){
+//         // console.log(numbersOfArray[i]);
+//         if(numbersOfArray[i]<smallest){
+//             smallest=numbersOfArray[i];
+//         }
+//     }
+//     return smallest;
+// }
+
+// console.log(smallestNumber([100,200,30,40,50,60]));
+
+
+// const arr=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18];
+// const even=arr.filter(number =>{
+//     return number % 2 == 0; 
+// })
+// console.log(even);
+
+// const arr2=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18];
+// const odds=arr.filter(number =>{
+//     return number % 2 != 0; 
+// })
+// console.log(odds);
+
+// const names=["Hasan","shuvo","sakib","tufan","Hasan","shuvo","sakib","tufan","Hasan","shuvo","sakib","tufan",];
+
+// function removeDuplicate(array){
+//     const uniquenames=[];
+//     for(let i=0;i<array.length;i++){
+//         // console.log(names[i]);
+//         if (uniquenames.indexOf(names[i])==-1){
+//             uniquenames.push(names[i])
+//         };
+//     };
+//     return uniquenames;
+// };
+// const output=removeDuplicate(names);
+// console.log(output);
+
+
+// let numbers =[0,2,3,4,5,6];
+// for(const number of numbers){
+//     console.log(number);
+// }
+// const text="Hello World";
+// for(const letter of text){
+//     console.log(letter);
+// }
+
+// let actorName = "modhumita";
+// function reversedLetter(word){
+//     let reversed = "";
+//     for (i=0;i<word.length;i++){
+//         // console.log(word[i]);
+//     let letter=word[i];
+//     reversed = letter + reversed;
+//     }
+//     return reversed;
+    
+// };
+// console.log(reversedLetter(actorName));
+
+// function add (num1, num2){
+//     console.log(num1, num2);
+//     return num1+num2;
+// };
+// const result=add(10,10);
+// console.log(result);
+// function add (num1, num2){
+//     console.log(num1, num2);
+//     return num1+num2;
+// };
+// const result=add(10+10);
+// console.log(result);
+
+ 
